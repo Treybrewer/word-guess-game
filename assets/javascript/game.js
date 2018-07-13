15f756b33c65c72585df9a3bc8034c41f9dfb9f8
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-07-13 14:56:32 -0400

    more hack javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,41 @@
 var correctAnswer = ['asia', 'africa', 'europe', 'northamerica', 'southamerica', 'australia', 'antarctica'];
 var randomAnswer = (Math.floor(Math.random() * correctAnswer.length));
 var choosenAnswer = correctAnswer[randomAnswer];
-var guessRemaining = [5];
+var guessRemaining = [choosenAnswer.length];
 var underscores = document.getElementById("underscores");
 var wronginput = [];
 var win = [0];
 var lose = [0];
 // create user input capture
 console.log(choosenAnswer);
-var inputArray = [];
- for (var i = 0; i < choosenAnswer.length; i++) {
- inputArray[i] = "_";
- }
- var remainingLetters = choosenAnswer.length;
+console.log(underscores);
+var userGuessarray = [];
+var wrong = [];
+document.onkeydown = function(event){
+    
+    console.log(event.key);
+    userGuessarray.push(event.key);
+    console.log(userGuessarray);
+    var wrongletters = document.getElementById("wrongletters");
+    
 
+    for (var i = 0; i < choosenAnswer.length; i++) {
+        if (userGuessarray === choosenAnswer[i]) {
+            underscores.innerHTML = userGuessarray;
+        }
+        else{
+            wrong.push(event.key);
+            wrong = wrongletters.innerHTML;
+        }
+    }
+       
+
+
+};
+
+
+
+ 
 // create a way to check the user input against the right answer
 
 
