b41a622c1ff98deef7aade88560c42cc1482fde9
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-07-16 00:08:57 -0400

    finally getting things to happen in js

--- assets/javascript/help.js
+++ assets/javascript/help.js
@@ -1,5 +1,5 @@
 // create variables and arrays
-var correctAnswer = ['asia', 'africa', 'europe', 'northamerica', 'southamerica', 'australia', 'antarctica'];
+var correctAnswer = ["asia", "africa", "europe", "northamerica", "southamerica", "australia", "antarctica"];
 var randomAnswer;
 var choosenAnswer;
 // var guessRemaining = choosenAnswer.length;
@@ -12,7 +12,9 @@ var lose = [0];// this can just be a number, no array
 // console.log(underscores);
 var userGuessarray = [];
 var letterAlreadyGuessed = [];
-var isInWord = false;
+var isInWord = true;
+var isNotinword = true;
+var letterGuessedArea = document.getElementById("wrongletters");
 
 
 // make a function to pick the word, put it in an array, make the display array
@@ -23,29 +25,28 @@ function start() {
     // choosenAnswer = "";
     // wronginput = [];
 
-   randomAnswer = (Math.floor(Math.random() * correctAnswer.length));
-   choosenAnswer = correctAnswer[randomAnswer];
-   console.log(choosenAnswer);
+    randomAnswer = (Math.floor(Math.random() * correctAnswer.length));
+    choosenAnswer = correctAnswer[randomAnswer];
+    console.log(choosenAnswer);
+    // need the length of the chosen word to display the number of blanks too
+    lengthOfWord = choosenAnswer.length;
 
-   // need the length of the chosen word to display the number of blanks too
-   lengthOfWord = choosenAnswer.length;
-
-   // a for loop here will run thru the length of word and push a "_" each time 
-   // this will be the displayed underline array you see
-//    for (var i = 0; i < lengthOfWord; i++) {
-//     userGuessarray.push(" ");  // put this code inside the for loop
-    
-    
-
-//    }
-   underscores.innerHTML = userGuessarray.join("");
-  
+    // a for loop here will run thru the length of word and push a "_" each time 
+    // this will be the displayed underline array you see
+    //    for (var i = 0; i < lengthOfWord; i++) {
+    //     userGuessarray.push(" ");  // put this code inside the for loop
 
 
-   // append the userGuessarray you just made above to the underscores var to display in the dom
+
+    //    }
+    underscores.innerHTML = userGuessarray.join("");
+    letterGuessedArea.innerHTML = wronginput.join("");
 
 
-   
+    // append the userGuessarray you just made above to the underscores var to display in the dom
+
+
+
 };
 
 start();
@@ -53,64 +54,33 @@ start();
 
 
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     // create if statement to check and see if it's in the alreadyguessed array
-   var currentLetter = underscores.innerHTML  //gets the current letter
-
-//    console.log(event.key);
-   userGuessarray.push(event.key);
-//    var wrongletters = document.getElementById("wrongletters");
-   
-   // this loop only determines if the letter is in the word-----------
-   //  if it is; the var isInWord becomes true, use this to continue the code.
-   for (var i = 0; i < choosenAnswer.length; i++) {
-       if (   currentLetter    === choosenAnswer[i]) {  // compares the current letter to the answerArray
-          isInWord = true;   // turn this true if the letter is in the word
-          userGuessarray[i] = currentLetter;
-       }
-       else{
-           // else the  isInWord var stays false;
-           
-       }
-   };
-   console.log(userGuessarray);
-
-   if(isInWord === true){
-    //   letterAlreadyGuessed.push(currentLetter);
-     isInWord === false;
-     for (var i = 0; i < choosenAnswer.length; i++) 
-        {
-         if (   currentLetter   === choosenAnswer[i]) {  
-            userGuessarray[i] = currentLetter;
-         }
+    var currentLetter = event.key;  //gets the current letter
         
-         else {
-            
-           }
-           
+    //    console.log(event.key);
+    
+
+    // this loop only determines if the letter is in the word-----------
+    //  if it is; the var isInWord becomes true, use this to continue the code.
+    for (var i = 0; i < choosenAnswer.length; i++) {
+        if (currentLetter === choosenAnswer[i]) {  // compares the current letter to the answerArray
+            isInWord = true;   // turn this true if the letter is in the word
+            userGuessarray[i] = currentLetter;
         }
-   }
-   console.log(wronginput);
-  
-    console.log(currentLetter);
-   
-   //-------------------------------------------------------------------
-   
-   
-   // we can work on this area after you get it dipsplaying the underscores
-
-
-   if(isInWord) {
-
-   }
-   
-
-   // wrong.push(event.key);
-   //         wrong = wrongletters.innerHTML;
-   underscores.innerHTML = userGuessarray;
+    };
+        if (currentLetter != choosenAnswer[i]) {
+            isNotinword = true;
+        }
+    
+    if (isNotinword = true) {
+        wronginput[i] = currentLetter;
+        wronginput.push(event.key);
+    };
+    console.log(userGuessarray);
+    console.log(wronginput);
+    underscores.innerHTML = userGuessarray;
+    letterGuessedArea.innerHTML = wronginput;
 
 };
 
-// Message Input
-
-// Message @Brian Smith
